b668e0501d3447ac07e4a33e5cc54d2bf868d5e9
shannonpearson <shannon.pearson@goverb.com>
2019-10-18 00:17:50 -0500

    adds admin routes

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const path = require('path');
 const cors = require('cors');
-const { connectDb } = require('./src/db/models/index');
+const connectDb = require('./src/db/models/index');
 const routes = require('./src/server/routes/index');
+const requiresAuthentication = require('./src/server/authenticationMiddleware');
 
 const app = express();
 
@@ -13,15 +14,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/users', routes.user);
 app.use('/donate', routes.donate);
+app.use('/admin', requiresAuthentication, routes.admin);
+app.use('/auth', routes.auth);
 
 app.get('*', (req, res) => {
   console.log('CATCHALL');
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-// app.listen(port, () => console.log(`app listening on port ${port}`));
 
 connectDb().then(async () => {
   console.log('Connected to mongo database');
