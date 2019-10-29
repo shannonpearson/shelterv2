7be19d63c7373b2fe6e2d3a3936307bc6665ac0d
shannonpearson <shannon.pearson@goverb.com>
2019-10-28 19:38:16 -0500

    fixes event display accordion

--- src/client/components/Blog/EventDisplay.js
+++ src/client/components/Blog/EventDisplay.js
@@ -13,22 +13,26 @@ const EventDisplay = (props) => {
   const formattedStartDate = format(new Date(startDate), 'MMM do, yyyy h:mm aa');
   const formattedEndDate = endDate ? format(new Date(endDate), 'MMM do, yyyy h:mm aa') : '';
   const dateString = `${formattedStartDate}${endDate ? ` - ${formattedEndDate}` : ''}`;
+  const defaultActiveKey = index === 0 ? 0 : null;
+
   return (
-    <Card className="event-card">
-      <Accordion.Toggle as={Card.Header} variant="link" eventKey={index}>
-        <div className="event-title">
-          {title}
-        </div>
-        <div className="event-date">
-          {dateString}
-        </div>
-      </Accordion.Toggle>
-      <Accordion.Collapse eventKey={index}>
-        <Card.Body>
-          <p className="event-description">{description}</p>
-        </Card.Body>
-      </Accordion.Collapse>
-    </Card>
+    <Accordion defaultActiveKey={defaultActiveKey}>
+      <Card className="event-card">
+        <Accordion.Toggle as={Card.Header} variant="link" eventKey={index}>
+          <div className="event-title">
+            {title}
+          </div>
+          <div className="event-date">
+            {dateString}
+          </div>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={index}>
+          <Card.Body>
+            <p className="event-description">{description}</p>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   );
 };
 
